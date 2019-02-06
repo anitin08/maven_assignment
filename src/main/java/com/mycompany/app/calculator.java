5bc3423960b8cf9c561afda3bbc3f48e8019e220
anitin08 <41472356+anitin08@users.noreply.github.com>
2019-02-07 02:50:08 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/app/calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nitin
+ */
+package com.mycompany.app;
+ import java.util.*;
+public class calculator {
+      
+    public static void main(String[] args)
+    {
+        System.out.println("this is working");
+               
+
+        }}
+
+   
+
